25561ccf18eb506d470eddcae9349f69bcd752c4
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-06-11 01:49:04 +0300

    some more stuff

--- src/svd_run2.lean
+++ src/svd_run2.lean
@@ -10,6 +10,11 @@ import tactic
 open matrix
 open_locale matrix big_operators complex_conjugate
 
+variables {m n p: Type*}
+variables [fintype m][fintype n][fintype p]
+variables [decidable_eq m][decidable_eq n][decidable_eq p]
+variables {R: Type*} [comm_ring R]
+
 lemma compl_subtypes_ne {z: Type*}[fintype z]{pn: z → Prop} :
      ∀ (i : {a // pn a}) (j : {a // ¬pn a}), (i: z) ≠ (j: z):= 
 begin
@@ -23,7 +28,7 @@ end
 variables {F: Type*}[field F][is_R_or_C F]
 def RηC := algebra_map ℝ ℂ
 
-lemma mul_star_self_nonneg {n : ℕ} (v: (fin n) → ℂ): 0 ≤ is_R_or_C.re((star v) ⬝ᵥ v) := 
+lemma mul_star_self_nonneg (v: n → ℂ): 0 ≤ is_R_or_C.re((star v) ⬝ᵥ v) := 
 begin
   simp_rw [is_R_or_C.re_to_complex, dot_product_comm, dot_product, 
     complex.re_sum, pi.star_apply, ← star_ring_end_apply, 
@@ -32,7 +37,7 @@ begin
   intros i hi, simp only [pow_nonneg, map_nonneg],
 end
 
-lemma conj_transpose_mul_self_is_pos_semidef {m n : ℕ} (A: matrix (fin m) (fin n) ℂ):
+lemma conj_transpose_mul_self_is_pos_semidef (A: matrix m n ℂ):
      matrix.pos_semidef (Aᴴ⬝A) 
 := begin
   split,
@@ -43,11 +48,44 @@ lemma conj_transpose_mul_self_is_pos_semidef {m n : ℕ} (A: matrix (fin m) (fin
   apply mul_star_self_nonneg,
 end
 
-lemma eigenvalues_nonneg_of_pos_semidef {n : ℕ} (A: matrix (fin n) (fin n) ℂ) 
-  (hAp: matrix.pos_semidef A) (i: fin n): 
+lemma eigenvalues_nonneg_of_pos_semidef (A: matrix n n ℂ) 
+  (hAp: matrix.pos_semidef A) (i: n): 
   0 ≤ hAp.1.eigenvalues i := begin
   rw  matrix.is_hermitian.eigenvalues_eq,
   apply hAp.2,
+end
+
+lemma matrix.left_mul_inj_of_invertible (P : matrix m m R) [invertible P] :
+  function.injective (λ (x : matrix m n R), P ⬝ x) := 
+begin
+  let hdetP_unit := matrix.is_unit_det_of_invertible P,
+  rintros x a hax, 
+  replace hax := congr_arg (λ (x : matrix m n R), P⁻¹ ⬝ x) hax,
+  simp only [inv_mul_cancel_left_of_invertible] at hax,
+  exact hax,
+end
+
+lemma ker_mat_mul_conj_transpose_mul_self (A: matrix m n ℂ)(B: matrix n p ℂ) :
+  (Aᴴ⬝A)⬝B = 0 ↔ A⬝B = 0 := begin
+  split,
+  intro h,
+  funext x y, rw pi.zero_apply,rw pi.zero_apply,
+  replace h := congr_arg (matrix.mul (Bᴴ)) h,
+  rw [matrix.mul_assoc, ← matrix.mul_assoc, ← conj_transpose_mul, matrix.mul_zero, 
+    ← matrix.ext_iff] at h,
+  replace h := h y y, simp_rw pi.zero_apply at h,
+  rw mul_apply at h, simp_rw conj_transpose_apply at h,
+  simp_rw [← star_ring_end_apply, mul_comm,
+    complex.mul_conj, ← complex.sq_abs, complex.ext_iff, complex.im_sum, complex.of_real_im, 
+    complex.re_sum, complex.of_real_re, complex.zero_re, complex.zero_im, 
+    finset.sum_const_zero, eq_self_iff_true, and_true] at h,
+  rw finset.sum_eq_zero_iff_of_nonneg at h,
+  simp_rw [finset.mem_univ, forall_true_left, pow_eq_zero_iff zero_lt_two, 
+    absolute_value.eq_zero] at h,
+  exact h x,
+  intro i, simp only [finset.mem_univ, pow_nonneg, map_nonneg, forall_true_left],
+  intro h, 
+  rw [matrix.mul_assoc, h, matrix.mul_zero],
 end
 
 
@@ -175,21 +213,44 @@ begin
       exact i.prop (h.symm), },
     rw mul_one_div_cancel,
     apply real.sqrt_ne_zero'.2 diagnz, },
+
+  have Vinv : Vᴴ⬝V = 1, 
+  sorry { apply_fun matrix.mul V,
+    rw ← matrix.mul_assoc,
+    rw matrix.is_hermitian.conj_transpose_eigenvector_matrix ,
+    rw matrix.is_hermitian.eigenvector_matrix_mul_inv,
+    rw [matrix.mul_one, matrix.one_mul],
+    exact matrix.left_mul_inj_of_invertible _, },
+  
+  have Vbh : V₁ᴴ ⬝ V₁ = 1 ∧ V₁ᴴ ⬝ V₂ = 0 ∧ V₂ᴴ ⬝ V₁ = 0 ∧ V₂ᴴ ⬝ V₂ = 1, 
+  sorry { rw [Vblock, reindex_apply, conj_transpose_submatrix, submatrix_mul_equiv,
+      from_blocks_conj_transpose, from_blocks_multiply] at Vinv,
+    change xV₁ with vec_empty at Vinv,
+    change xV₂ with vec_empty at Vinv,
+    simp only [empty_mul_empty, add_zero] at Vinv,
+    apply_fun reindex e.symm e.symm at Vinv,
+    rw reindex_apply at Vinv,
+    simp only [equiv.symm_symm, submatrix_submatrix, equiv.symm_comp_self, 
+      submatrix_id_id, reindex_apply, submatrix_one_equiv] at Vinv,
+    rw [← from_blocks_one, from_blocks_inj] at Vinv, 
+    exact Vinv},
+
+  
   let U₁ := A⬝V₁⬝((Sσ⁻¹).map RηC),
   have V₁inv : V₁ᴴ⬝V₁ = 1, sorry,
   have U₁inv : U₁ᴴ⬝U₁ = 1, sorry,
   
-  have: U₁⬝((Sσ).map RηC) = A ⬝ V₁, 
+  have U₁Sσ : U₁⬝((Sσ).map RηC) = A ⬝ V₁, 
   sorry { change U₁ with A⬝V₁⬝((Sσ⁻¹).map RηC),
     rw [matrix.mul_assoc, ← matrix.map_mul, nonsing_inv_mul, matrix.map_one, matrix.mul_one],
     exact map_zero RηC, 
     exact map_one RηC, 
     exact (is_unit_det_of_invertible Sσ), },
   
-  have : A⬝V₂ = 0, {
-    clear Sσinv,
-    extract_goal,
-  },
+  have AV₂ : A⬝V₂ = 0, 
+  sorry { suffices h : Aᴴ⬝A⬝V₂ = 0,
+    apply (ker_mat_mul_conj_transpose_mul_self _ _).1 h,
+    rw [reducedSpectral, matrix.mul_assoc, (Vbh.2.1), matrix.mul_zero], },
 
 --   {
 --      calc U₁ᴴ⬝U₁ = 
